ae8b1d9af60d1a5cb358e3345f4703a16afdca3c
gonzaloSarmientoSejas <gonzalo@kplian.com>
2013-04-25 16:19:59 -0400

    -#131 Titulo: Preparar datos de prueba Duracion: 4 horas Descripcion: se arreglaron algunos datos para la fusion del sistema de almacenes y el sistema de adquisiciones de Boa

--- base/test_data.sql
+++ base/test_data.sql
@@ -2,11 +2,11 @@
 -- DEPARTAMENTOS DE TESORERIA
 -----------------------------------------------
 
-INSERT INTO param.tdepto ("id_usuario_reg", "id_usuario_mod", "fecha_reg", "fecha_mod", "estado_reg", "id_subsistema", "codigo", "nombre", "nombre_corto")
-VALUES (1, NULL, E'2013-04-05 00:00:00', E'2013-04-05 06:20:20.098', E'activo', 11, E'TES-CEN', E'TEsoreria Central', E'');
+INSERT INTO param.tdepto ("id_usuario_reg", "id_usuario_mod", "fecha_reg", "fecha_mod", "estado_reg", "id_depto", "id_subsistema", "codigo", "nombre", "nombre_corto")
+VALUES (1, NULL, E'2013-04-05 00:00:00', E'2013-04-05 06:20:20.098', E'activo', 3, 11, E'TES-CEN', E'TEsoreria Central', E'');
 
-INSERT INTO param.tdepto ("id_usuario_reg", "id_usuario_mod", "fecha_reg", "fecha_mod", "estado_reg", "id_subsistema", "codigo", "nombre", "nombre_corto")
-VALUES (1, NULL, E'2013-04-05 00:00:00', E'2013-04-05 06:20:49.279', E'activo', 11, E'TES-ARG', E'Tesoreria Argentina', E'');
+INSERT INTO param.tdepto ("id_usuario_reg", "id_usuario_mod", "fecha_reg", "fecha_mod", "estado_reg", "id_depto", "id_subsistema", "codigo", "nombre", "nombre_corto")
+VALUES (1, NULL, E'2013-04-05 00:00:00', E'2013-04-05 06:20:49.279', E'activo', 4, 11, E'TES-ARG', E'Tesoreria Argentina', E'');
 
 -------------------------------------------
 -- INICIO ROLES 
