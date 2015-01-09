7be98f7151211e68c557c873c7d960a4f8288ec2
dora <dcastro@boa.bo>
2015-01-09 16:05:09 -0400

    integracion de contratos con obligaciones

---
+++ base/funciones/tes.f_lista_funcionario_registro_op.sql
@@ -0,0 +1,148 @@
+--------------- SQL ---------------
+
+CREATE OR REPLACE FUNCTION tes.f_lista_funcionario_registro_op (
+  p_id_usuario integer,
+  p_id_tipo_estado integer,
+  p_fecha date = now(),
+  p_id_estado_wf integer = NULL::integer,
+  p_count boolean = false,
+  p_limit integer = 1,
+  p_start integer = 0,
+  p_filtro varchar = '0=0'::character varying
+)
+RETURNS SETOF record AS
+$body$
+/**************************************************************************
+ SISTEMA ENDESIS - SISTEMA DE ...
+***************************************************************************
+ SCRIPT: 		tes.f_lista_funcionario_registro_op
+ DESCRIPCIÓN: 	Lista el funcionario que registra la Obligacion de pago
+ AUTOR: 		Rensi Arteaga Copari
+ FECHA:			08/01/2015
+ COMENTARIOS:	
+***************************************************************************
+ HISTORIA DE MODIFICACIONES:
+
+ DESCRIPCIÓN:
+ AUTOR:       
+ FECHA:      
+
+***************************************************************************/
+
+-------------------------
+-- CUERPO DE LA FUNCIÓN --
+--------------------------
+
+-- PARÁMETROS FIJOS
+/*
+
+
+  p_id_usuario integer,                                identificador del actual usuario de sistema
+  p_id_tipo_estado integer,                            idnetificador del tipo estado del que se quiere obtener el listado de funcionario  (se correponde con tipo_estado que le sigue a id_estado_wf proporcionado)                       
+  p_fecha date = now(),                                fecha  --para verificar asginacion de cargo con organigrama
+  p_id_estado_wf integer = NULL::integer,              identificaro de estado_wf actual en el proceso_wf
+  p_count boolean = false,                             si queremos obtener numero de funcionario = true por defecto false
+  p_limit integer = 1,                                 los siguiente son parametros para filtrar en la consulta
+  p_start integer = 0,
+  p_filtro varchar = '0=0'::character varying
+
+
+
+
+*/
+
+DECLARE
+	g_registros  		record;
+    v_depto_asignacion    varchar;
+    v_nombre_depto_func_list   varchar;
+    
+    v_consulta varchar;
+    v_nombre_funcion varchar;
+    v_resp varchar;
+    
+    
+     v_cad_ep varchar;
+     v_cad_uo varchar;
+     v_id_funcionario_gerente   integer;
+    
+    v_a_eps varchar[];
+    v_a_uos varchar[];
+    v_uos_eps varchar;
+    v_size    integer;
+    v_i       integer;
+    v_id_usuario_reg  integer;
+    v_id_funcionario integer;
+
+BEGIN
+    
+    v_nombre_funcion ='tes.f_lista_funcionario_registro_op';
+
+    --recuperamos la la opbligacion de pago a partir del is_estado_wf del la obligacion
+    
+    select 
+      op.id_usuario_reg
+    into
+      v_id_usuario_reg
+    from tes.tobligacion_pago op 
+    where op.id_estado_wf = p_id_estado_wf;
+    
+    
+    --obtiene el funciono que registros la obligacion
+    
+    select
+    f.id_funcionario
+    into
+    v_id_funcionario
+    from segu.tusuario u 
+    inner join segu.tpersona p on u.id_persona = p.id_persona
+    inner join orga.tfuncionario f on f.id_persona = p.id_persona
+    where u.id_usuario = v_id_usuario_reg;
+   
+    IF not p_count then
+    
+             v_consulta:='SELECT
+                            fun.id_funcionario,
+                            fun.desc_funcionario1 as desc_funcionario,
+                            ''Gerente''::text  as desc_funcionario_cargo,
+                            1 as prioridad
+                         FROM orga.vfuncionario fun WHERE fun.id_funcionario = '||COALESCE(v_id_funcionario,0)::varchar||'
+                         and '||p_filtro||'
+                         limit '|| p_limit::varchar||' offset '||p_start::varchar; 
+     
+              
+                   FOR g_registros in execute (v_consulta)LOOP     
+                     RETURN NEXT g_registros;
+                   END LOOP;
+                      
+      ELSE
+                  v_consulta='select
+                                  COUNT(fun.id_funcionario) as total
+                                 FROM orga.vfuncionario fun WHERE fun.id_funcionario = '||COALESCE(v_id_funcionario,0)::varchar||'
+                                 and '||p_filtro;   
+                                          
+                   FOR g_registros in execute (v_consulta)LOOP     
+                     RETURN NEXT g_registros;
+                   END LOOP;
+                       
+                       
+    END IF;
+               
+        
+       
+EXCEPTION
+					
+	WHEN OTHERS THEN
+			v_resp='';
+			v_resp = pxp.f_agrega_clave(v_resp,'mensaje',SQLERRM);
+			v_resp = pxp.f_agrega_clave(v_resp,'codigo_error',SQLSTATE);
+			v_resp = pxp.f_agrega_clave(v_resp,'procedimientos',v_nombre_funcion);
+			raise exception '%',v_resp;
+
+				        
+END;
+$body$
+LANGUAGE 'plpgsql'
+VOLATILE
+CALLED ON NULL INPUT
+SECURITY INVOKER
+COST 100 ROWS 1000;
---
+++ base/funciones/tes.f_validar_contrato.sql
@@ -0,0 +1,61 @@
+--------------- SQL ---------------
+
+CREATE OR REPLACE FUNCTION tes.f_validar_contrato (
+  p_id_obligacion_pago integer
+)
+RETURNS boolean AS
+$body$
+DECLARE
+   
+    v_resp		            varchar;
+	v_nombre_funcion        text;
+	v_mensaje_error         text;
+    v_registros_op			record;
+    v_count 				integer;
+    
+BEGIN
+	v_nombre_funcion = 'tes.f_validar_contrato';
+    
+    -- verificamso si tiene por lo menos un concepto de gasto que requeire contrato
+    select 
+      op.id_contrato,
+      op.tipo_obligacion
+    into 
+      v_registros_op
+    from tes.tobligacion_pago op
+    where op.id_obligacion_pago = p_id_obligacion_pago;
+    
+    select
+     count(od.id_obligacion_det) 
+    into
+     v_count
+    from tes.tobligacion_det od
+    inner join  param.tconcepto_ingas ci on ci.id_concepto_ingas =  od.id_concepto_ingas
+    where od.id_obligacion_pago = p_id_obligacion_pago and  'contrato' = ANY(ci.sw_autorizacion);
+    
+    --TODO, validaciones  adiconales sobre el  contrtao, fecha, montos  etc....
+    --TODO validar que obligacione de adquisicioens deberian tener contrato 
+    
+    IF v_count > 1  and v_registros_op.tipo_obligacion = 'pago_directo' THEN
+       IF v_registros_op.id_cotrato is null then
+              raise exception 'Para esta obligacion de pago es requerido un contrato de referencia';
+       END IF;
+    END IF;
+    
+    return True;
+EXCEPTION
+				
+	WHEN OTHERS THEN
+		v_resp='';
+		v_resp = pxp.f_agrega_clave(v_resp,'mensaje',SQLERRM);
+		v_resp = pxp.f_agrega_clave(v_resp,'codigo_error',SQLSTATE);
+		v_resp = pxp.f_agrega_clave(v_resp,'procedimientos',v_nombre_funcion);
+		raise exception '%',v_resp;
+				        
+END;
+$body$
+LANGUAGE 'plpgsql'
+VOLATILE
+CALLED ON NULL INPUT
+SECURITY INVOKER
+COST 100;
