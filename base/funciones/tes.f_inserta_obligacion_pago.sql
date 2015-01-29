745dff391ee6aaf14dbb688e58babf1ce26822ce
root <root@localhost.localdomain>
2015-01-28 20:04:29 -0400

    adicionando validacion temporal por cambio de gestion

--- base/funciones/tes.f_inserta_obligacion_pago.sql
+++ base/funciones/tes.f_inserta_obligacion_pago.sql
@@ -1,12 +1,9 @@
---------------- SQL ---------------
-
 CREATE OR REPLACE FUNCTION tes.f_inserta_obligacion_pago (
   p_administrador integer,
   p_id_usuario integer,
   p_hstore public.hstore
 )
-RETURNS varchar AS
-$body$
+RETURNS varchar AS'
 /**************************************************************************
  SISTEMA:		Adquisiciones
  FUNCION: 		tes.f_inserta_obligacion_pago
@@ -57,84 +54,87 @@ DECLARE
 			    
 BEGIN
 
-    v_nombre_funcion = 'f_inserta_obligacion_pago';
+    v_nombre_funcion = ''f_inserta_obligacion_pago'';
 
     /*
     HSTORE  PARAMETERS
-            (p_hstore->'fecha')::date
-            (p_hstore->'tipo_obligacion')::varchar
-            (p_hstore->'id_funcionario')::integer
-            (p_hstore->'_id_usuario_ai')::integer,
-            (p_hstore->'_nombre_usuario_ai')::varchar,
-            (p_hstore->'id_funcionario')::integer,
-            (p_hstore->'id_depto')::integer,
-            (p_hstore->'obs')::varchar,
-            (p_hstore->'id_proveedor')::integer,
-			(p_hstore->'tipo_obligacion')::varchar,
-			(p_hstore->'id_moneda')::integer,
-			(p_hstore->'obs')::varchar,
-			(p_hstore->'id_funcionario')::integer,
-			(p_hstore->'id_depto')::integer,
-			(p_hstore->'tipo_cambio_conv')::numeric,
-            (p_hstore->'pago_variable')::varchar,
-            (p_hstore->'total_nro_cuota')::integer,
-            (p_hstore->'fecha_pp_ini')::date,
-            (p_hstore->'rotacion')::integer,
-            (p_hstore->'id_plantilla')::integer,
-            (p_hstore->'_id_usuario_ai')::integer,
-            (p_hstore->'_nombre_usuario_ai')::varchar,
-            (p_hstore->'tipo_anticipo')::varchar,
+            (p_hstore->''fecha'')::date
+            (p_hstore->''tipo_obligacion'')::varchar
+            (p_hstore->''id_funcionario'')::integer
+            (p_hstore->''_id_usuario_ai'')::integer,
+            (p_hstore->''_nombre_usuario_ai'')::varchar,
+            (p_hstore->''id_funcionario'')::integer,
+            (p_hstore->''id_depto'')::integer,
+            (p_hstore->''obs'')::varchar,
+            (p_hstore->''id_proveedor'')::integer,
+			(p_hstore->''tipo_obligacion'')::varchar,
+			(p_hstore->''id_moneda'')::integer,
+			(p_hstore->''obs'')::varchar,
+			(p_hstore->''id_funcionario'')::integer,
+			(p_hstore->''id_depto'')::integer,
+			(p_hstore->''tipo_cambio_conv'')::numeric,
+            (p_hstore->''pago_variable'')::varchar,
+            (p_hstore->''total_nro_cuota'')::integer,
+            (p_hstore->''fecha_pp_ini'')::date,
+            (p_hstore->''rotacion'')::integer,
+            (p_hstore->''id_plantilla'')::integer,
+            (p_hstore->''_id_usuario_ai'')::integer,
+            (p_hstore->''_nombre_usuario_ai'')::varchar,
+            (p_hstore->''tipo_anticipo'')::varchar,
     
     */
     
+    if ((p_hstore->''fecha'')::date < ''01-01-2015'' and p_administrador = 0) then
+    	raise exception ''No se puede hacer una solicitud con gestion 2014, porfavor consulte con el administrador'';
+    end if;
 
     --determina la fecha del periodo
         
          select id_periodo into v_id_periodo from
                         param.tperiodo per 
-                       where per.fecha_ini <= (p_hstore->'fecha')::date 
-                         and per.fecha_fin >= (p_hstore->'fecha')::date
+                       where per.fecha_ini <= (p_hstore->''fecha'')::date 
+                         and per.fecha_fin >= (p_hstore->''fecha'')::date
                          limit 1 offset 0;
         
         
-        IF   (p_hstore->'tipo_obligacion')::varchar = 'adquisiciones'    THEN
-             raise exception 'Los pagos de adquisiciones tienen que ser habilitados desde el sistema de adquisiciones';   
+        IF   (p_hstore->''tipo_obligacion'')::varchar = ''adquisiciones''    THEN
+             raise exception ''Los pagos de adquisiciones tienen que ser habilitados desde el sistema de adquisiciones'';   
        
-        ELSIF   (p_hstore->'tipo_obligacion')::varchar ='pago_directo'    THEN
+        ELSIF   (p_hstore->''tipo_obligacion'')::varchar =''pago_directo''    THEN
               
-                v_tipo_documento = 'PGD';
+                v_tipo_documento = ''PGD'';
                 
                   --obtener correlativo
                  v_num =   param.f_obtener_correlativo(
-                          'PGD', 
+                          ''PGD'', 
                            v_id_periodo,-- par_id, 
                            NULL, --id_uo 
-                           (p_hstore->'id_depto')::integer,    -- id_depto
+                           (p_hstore->''id_depto'')::integer,    -- id_depto
                            p_id_usuario, 
-                           'TES', 
+                           ''TES'', 
                            NULL);
                            
         
-                v_codigo_proceso_macro = 'TES-PD'; 
+                v_codigo_proceso_macro = ''TES-PD''; 
                 
                 --si el funcionario que solicita es un gerente .... es el mimso encargado de aprobar
                 
                  IF exists(select 1 from orga.tuo_funcionario uof 
-                           inner join orga.tuo uo on uo.id_uo = uof.id_uo and uo.estado_reg = 'activo'
+                           inner join orga.tuo uo on uo.id_uo = uof.id_uo and uo.estado_reg = ''activo''
                            inner join orga.tnivel_organizacional no on no.id_nivel_organizacional = uo.id_nivel_organizacional and no.numero_nivel in (1,2)
-                           where  uof.estado_reg = 'activo' and  uof.id_funcionario = (p_hstore->'id_funcionario')::integer ) THEN
+                           where  uof.estado_reg = ''activo'' and  uof.id_funcionario = (p_hstore->''id_funcionario'')::integer ) THEN
                   
-                      va_id_funcionario_gerente[1] = (p_hstore->'id_funcionario')::integer;
+                      va_id_funcionario_gerente[1] = (p_hstore->''id_funcionario'')::integer;
                  
                  ELSE
                     --si tiene funcionario identificar el gerente correspondientes
-                    IF (p_hstore->'id_funcionario')::integer  is not NULL THEN
+                    IF (p_hstore->''id_funcionario'')::integer  is not NULL THEN
                     
                         SELECT  
                            pxp.aggarray(id_funcionario) 
                          into
                            va_id_funcionario_gerente
-                         FROM orga.f_get_aprobadores_x_funcionario((p_hstore->'fecha')::date,  (p_hstore->'id_funcionario')::integer , 'todos', 'si', 'todos', 'ninguno') AS (id_funcionario integer);      
+                         FROM orga.f_get_aprobadores_x_funcionario((p_hstore->''fecha'')::date,  (p_hstore->''id_funcionario'')::integer , ''todos'', ''si'', ''todos'', ''ninguno'') AS (id_funcionario integer);      
                         --NOTA el valor en la primera posicion del array es el genre de menor nivel
                     END IF;  
                 END IF;
@@ -142,14 +142,14 @@ BEGIN
               
         ELSE
           
-          raise exception 'falta agregar la funcionalidad'; 
+          raise exception ''falta agregar la funcionalidad''; 
               
         END IF;
        
        
-        IF (v_num is NULL or v_num ='') THEN
+        IF (v_num is NULL or v_num ='''') THEN
         
-          raise exception 'No se pudo obtener un numero correlativo para la obligación';
+          raise exception ''No se pudo obtener un numero correlativo para la obligación'';
         
         END IF;
         
@@ -165,7 +165,7 @@ BEGIN
         
         If v_id_proceso_macro is NULL THEN
         
-           raise exception 'El proceso macro  de codigo % no esta configurado en el sistema WF',v_codigo_proceso_macro;  
+           raise exception ''El proceso macro  de codigo % no esta configurado en el sistema WF'',v_codigo_proceso_macro;  
         
         END IF;
         
@@ -181,17 +181,17 @@ BEGIN
             into v_codigo_tipo_proceso
         from  wf.ttipo_proceso tp 
         where   tp.id_proceso_macro = v_id_proceso_macro
-                and tp.estado_reg = 'activo' and tp.inicio = 'si';
+                and tp.estado_reg = ''activo'' and tp.inicio = ''si'';
             
          
         IF v_codigo_tipo_proceso is NULL THEN
         
-           raise exception 'No existe un proceso inicial para el proceso macro indicado % (Revise la configuración)',v_codigo_proceso_macro;
+           raise exception ''No existe un proceso inicial para el proceso macro indicado % (Revise la configuración)'',v_codigo_proceso_macro;
         
         END IF;
         
         
-        v_anho = (date_part('year', (p_hstore->'fecha')::date))::integer;
+        v_anho = (date_part(''year'', (p_hstore->''fecha'')::date))::integer;
 			
             select 
              ges.id_gestion
@@ -206,7 +206,7 @@ BEGIN
        s.id_subsistema
        into 
        v_id_subsistema
-       from segu.tsubsistema s where s.codigo = 'ADQ';
+       from segu.tsubsistema s where s.codigo = ''ADQ'';
     
         
         -- inciar el tramite en el sistema de WF
@@ -223,17 +223,17 @@ BEGIN
                 
           FROM wf.f_inicia_tramite(
                p_id_usuario,
-               (p_hstore->'_id_usuario_ai')::integer,
-               (p_hstore->'_nombre_usuario_ai')::varchar,
+               (p_hstore->''_id_usuario_ai'')::integer,
+               (p_hstore->''_nombre_usuario_ai'')::varchar,
                v_id_gestion, 
                v_codigo_tipo_proceso, 
-               (p_hstore->'id_funcionario')::integer,
-               (p_hstore->'id_depto')::integer,
-               'Obligacion de pago ('||v_num||') '||(p_hstore->'obs')::varchar,
+               (p_hstore->''id_funcionario'')::integer,
+               (p_hstore->''id_depto'')::integer,
+               ''Obligacion de pago (''||v_num||'') ''||(p_hstore->''obs'')::varchar,
                v_num );
             
            
-            -- raise exception 'estado %',v_codigo_estado;
+            -- raise exception ''estado %'',v_codigo_estado;
              
       
         	--Sentencia de la insercion
@@ -272,18 +272,18 @@ BEGIN
             id_contrato
             
           	) values(
-			(p_hstore->'id_proveedor')::integer,
+			(p_hstore->''id_proveedor'')::integer,
 			v_codigo_estado,
-			(p_hstore->'tipo_obligacion')::varchar,
-			(p_hstore->'id_moneda')::integer,
-			(p_hstore->'obs')::varchar,
+			(p_hstore->''tipo_obligacion'')::varchar,
+			(p_hstore->''id_moneda'')::integer,
+			(p_hstore->''obs'')::varchar,
 			--v_parametros.porc_retgar,
 			v_id_subsistema,
-			(p_hstore->'id_funcionario')::integer,
-			'activo',
+			(p_hstore->''id_funcionario'')::integer,
+			''activo'',
 			--v_parametros.porc_anticipo,
 			v_id_estado_wf,
-			(p_hstore->'id_depto')::integer,
+			(p_hstore->''id_depto'')::integer,
 			v_num_tramite,
 			v_id_proceso_wf,
 			now(),
@@ -291,19 +291,19 @@ BEGIN
 			null,
 			null,
             v_num,
-            (p_hstore->'fecha')::date,
+            (p_hstore->''fecha'')::date,
             v_id_gestion,
-            (p_hstore->'tipo_cambio_conv')::numeric,
-            (p_hstore->'pago_variable')::varchar,
-            (p_hstore->'total_nro_cuota')::integer,
-            (p_hstore->'fecha_pp_ini')::date,
-            (p_hstore->'rotacion')::integer,
-            (p_hstore->'id_plantilla')::integer,
-            (p_hstore->'_id_usuario_ai')::integer,
-            (p_hstore->'_nombre_usuario_ai')::varchar,
-            (p_hstore->'tipo_anticipo')::varchar,
+            (p_hstore->''tipo_cambio_conv'')::numeric,
+            (p_hstore->''pago_variable'')::varchar,
+            (p_hstore->''total_nro_cuota'')::integer,
+            (p_hstore->''fecha_pp_ini'')::date,
+            (p_hstore->''rotacion'')::integer,
+            (p_hstore->''id_plantilla'')::integer,
+            (p_hstore->''_id_usuario_ai'')::integer,
+            (p_hstore->''_nombre_usuario_ai'')::varchar,
+            (p_hstore->''tipo_anticipo'')::varchar,
              va_id_funcionario_gerente[1],
-            (p_hstore->'id_contrato')::integer
+            (p_hstore->''id_contrato'')::integer
 							
 			)RETURNING id_obligacion_pago into v_id_obligacion_pago;
             
@@ -318,7 +318,7 @@ BEGIN
             -------------------------------------
             
             --  Si el la referencia al contrato esta presente ..  copiar el documento de contrato
-            IF (p_hstore->'id_contrato')::integer  is not  NULL THEN
+            IF (p_hstore->''id_contrato'')::integer  is not  NULL THEN
                  --con el ide de contrato obtenet el id_proceso_wf
                  SELECT
                    con.id_proceso_wf,
@@ -329,7 +329,7 @@ BEGIN
                   v_registros_con
                  FROM leg.tcontrato con
                  INNER JOIN wf.tproceso_wf pwf on pwf.id_proceso_wf = con.id_proceso_wf
-                 WHERE con.id_contrato = (p_hstore->'id_contrato')::integer;
+                 WHERE con.id_contrato = (p_hstore->''id_contrato'')::integer;
                 
                   -- octenemos el documentos constro del origen
                
@@ -339,7 +339,7 @@ BEGIN
                    v_registros_documento
                   FROM wf.tdocumento_wf d
                   INNER JOIN wf.ttipo_documento td on td.id_tipo_documento = d.id_tipo_documento
-                  WHERE td.codigo = 'CONTRATO' and 
+                  WHERE td.codigo = ''CONTRATO'' and 
                         d.id_proceso_wf = v_registros_con.id_proceso_wf;
                  
                    -- copiamos el link de referencia del contrato de la obligacion de pago
@@ -349,7 +349,7 @@ BEGIN
                      v_id_documento_wf_op
                     from wf.tdocumento_wf dwf
                     inner  join  wf.ttipo_documento td on td.id_tipo_documento = dwf.id_tipo_documento
-                    where td.codigo = 'CONTRATO'  and dwf.id_proceso_wf = v_id_proceso_wf;
+                    where td.codigo = ''CONTRATO''  and dwf.id_proceso_wf = v_id_proceso_wf;
                  
                     UPDATE 
                       wf.tdocumento_wf  
@@ -373,9 +373,9 @@ BEGIN
             END IF;
             
              --Definicion de la respuesta
-			 v_resp = pxp.f_agrega_clave(v_resp,'mensaje','Obligaciones de Pago almacenado(a) con exito (id_obligacion_pago'||v_id_obligacion_pago||')'); 
-             v_resp = pxp.f_agrega_clave(v_resp,'id_obligacion_pago',v_id_obligacion_pago::varchar);
-             v_resp = pxp.f_agrega_clave(v_resp,'id_gestion',v_id_gestion::varchar);
+			 v_resp = pxp.f_agrega_clave(v_resp,''mensaje'',''Obligaciones de Pago almacenado(a) con exito (id_obligacion_pago''||v_id_obligacion_pago||'')''); 
+             v_resp = pxp.f_agrega_clave(v_resp,''id_obligacion_pago'',v_id_obligacion_pago::varchar);
+             v_resp = pxp.f_agrega_clave(v_resp,''id_gestion'',v_id_gestion::varchar);
 
             --Devuelve la respuesta
             return v_resp;
@@ -388,15 +388,14 @@ BEGIN
 EXCEPTION
 				
 	WHEN OTHERS THEN
-		v_resp='';
-		v_resp = pxp.f_agrega_clave(v_resp,'mensaje',SQLERRM);
-		v_resp = pxp.f_agrega_clave(v_resp,'codigo_error',SQLSTATE);
-		v_resp = pxp.f_agrega_clave(v_resp,'procedimientos',v_nombre_funcion);
-		raise exception '%',v_resp;
+		v_resp='''';
+		v_resp = pxp.f_agrega_clave(v_resp,''mensaje'',SQLERRM);
+		v_resp = pxp.f_agrega_clave(v_resp,''codigo_error'',SQLSTATE);
+		v_resp = pxp.f_agrega_clave(v_resp,''procedimientos'',v_nombre_funcion);
+		raise exception ''%'',v_resp;
 				        
 END;
-$body$
-LANGUAGE 'plpgsql'
+'LANGUAGE 'plpgsql'
 VOLATILE
 CALLED ON NULL INPUT
 SECURITY INVOKER
