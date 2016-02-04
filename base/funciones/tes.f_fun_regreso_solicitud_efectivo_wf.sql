a0ba0c200b2d5660f37dc3ba349969f0b7e0e23a
Jaime Rivera <jriverarojas@gmail.com>
2016-02-04 09:47:44 -0400

    funciones faltantes

---
+++ base/funciones/tes.f_fun_regreso_solicitud_efectivo_wf.sql
@@ -0,0 +1,61 @@
+CREATE OR REPLACE FUNCTION tes.f_fun_regreso_solicitud_efectivo_wf (
+  p_id_usuario integer,
+  p_id_usuario_ai integer,
+  p_usuario_ai varchar,
+  p_id_estado_wf integer,
+  p_id_proceso_wf integer,
+  p_codigo_estado varchar
+)
+RETURNS boolean AS
+$body$
+/*
+*
+*  Autor:   GSS
+*  DESC:    funcion que actualiza los estados despues del registro de un retroceso en caja
+*  Fecha:   09/12/2015
+*
+*/
+
+DECLARE
+
+	v_nombre_funcion   	text;
+    v_resp    			varchar;
+    v_mensaje varchar;
+   
+	
+    
+BEGIN
+
+	v_nombre_funcion = 'tes.f_fun_regreso_solicitud_efectivo_wf';
+    
+    -- actualiza estado en la solicitud
+    update tes.tsolicitud_efectivo  se set 
+         id_estado_wf =  p_id_estado_wf,
+         estado = p_codigo_estado,
+         id_usuario_mod=p_id_usuario,
+         fecha_mod=now(),
+         id_usuario_ai = p_id_usuario_ai,
+         usuario_ai = p_usuario_ai
+    where id_proceso_wf = p_id_proceso_wf;
+   
+      
+
+RETURN   TRUE;
+
+
+
+EXCEPTION
+					
+	WHEN OTHERS THEN
+			v_resp='';
+			v_resp = pxp.f_agrega_clave(v_resp,'mensaje',SQLERRM);
+			v_resp = pxp.f_agrega_clave(v_resp,'codigo_error',SQLSTATE);
+			v_resp = pxp.f_agrega_clave(v_resp,'procedimientos',v_nombre_funcion);
+			raise exception '%',v_resp;
+END;
+$body$
+LANGUAGE 'plpgsql'
+VOLATILE
+CALLED ON NULL INPUT
+SECURITY INVOKER
+COST 100;
---
+++ vista/caja/CajaSolicitud.php
@@ -0,0 +1,133 @@
+<?php
+/**
+*@package pXP
+*@file CajaSolicitud.php
+*@author  gsarmiento
+*@date 29-01-2016
+*@description Archivo con la interfaz de usuario que permite 
+*solicitar la apertura de caja chica
+*
+*/
+header("content-type: text/javascript; charset=UTF-8"); 
+?>
+<script>
+Phx.vista.CajaSolicitud = {
+    require:'../../../sis_tesoreria/vista/caja/Caja.php',
+    requireclase:'Phx.vista.Caja',
+    title:'Solicitud Caja',
+    nombreVista: 'caja',
+    
+    constructor: function(config) {
+      
+       Phx.vista.CajaSolicitud.superclass.constructor.call(this,config);
+          
+	   
+	   this.addButton('diagrama_gantt',{text:'Gantt',iconCls: 'bgantt',disabled:false,handler:diagramGantt,tooltip: '<b>Diagrama Gantt de proceso macro</b>'});
+       this.addButton('btnAbrirCerrar',
+			{
+				text: 'Cerrar/Abrir',
+				iconCls: 'block',
+				disabled: false,
+				handler: this.abrirCerrarCaja,
+				tooltip: '<b>Cerrar/Abrir</b><br/>Cerrar/Abrir caja'
+			}
+		);
+		
+		function diagramGantt(){            
+            var data=this.sm.getSelected().data.id_proceso_wf;
+            Phx.CP.loadingShow();
+            Ext.Ajax.request({
+                url:'../../sis_workflow/control/ProcesoWf/diagramaGanttTramite',
+                params:{'id_proceso_wf':data},
+                success:this.successExport,
+                failure: this.conexionFailure,
+                timeout:this.timeout,
+                scope:this
+            });         
+        }
+		
+        this.load({params:{start:0, limit:this.tam_pag, tipo_interfaz: this.nombreVista}})
+    },
+	
+		
+	preparaMenu:function(n){
+         var data = this.getSelectedData();
+         
+         if(data.estado_proceso == 'borrador'){  
+		    this.getBoton('btnAbrirCerrar').enable();
+			this.getBoton('edit').enable();
+			this.getBoton('del').enable();
+         }
+		 else{
+			 this.getBoton('edit').disable();
+			 this.getBoton('del').disable();
+			 if(data.estado =='abierto')
+				 this.getBoton('btnAbrirCerrar').enable();			 
+			 else
+				this.getBoton('btnAbrirCerrar').disable();
+		 }		 
+     },
+    
+	 abrirCerrarCaja:function(){
+		var rec=this.sm.getSelected();
+		var NumSelect=this.sm.getCount();
+		
+		if(NumSelect != 0)
+		{	
+		  if(rec.data.estado=='cerrado'){
+			  this.objWizard = Phx.CP.loadWindows('../../../sis_workflow/vista/estado_wf/FormEstadoWf.php',
+							'Estado de Wf',
+							{
+								modal:true,
+								width:700,
+								height:450
+							}, {data:{
+								   id_estado_wf:rec.data.id_estado_wf,
+								   id_proceso_wf:rec.data.id_proceso_wf								  
+								}}, this.idContenedor,'FormEstadoWf',
+							{
+								config:[{
+										  event:'beforesave',
+										  delegate: this.onSaveWizard												  
+										}],
+								
+								scope:this
+							 });
+		  }
+		}
+		else
+		{
+			Ext.MessageBox.alert('Alerta', 'Antes debe seleccionar un item.');
+		}					   
+	},
+		
+	onSaveWizard:function(wizard,resp){
+		Phx.CP.loadingShow();
+		
+		Ext.Ajax.request({
+			url:'../../sis_tesoreria/control/Caja/siguienteEstadoCaja',
+			params:{
+					
+				id_proceso_wf_act:  resp.id_proceso_wf_act,
+				id_estado_wf_act:   resp.id_estado_wf_act,
+				id_tipo_estado:     resp.id_tipo_estado,
+				id_funcionario_wf:  resp.id_funcionario_wf,
+				id_depto_wf:        resp.id_depto_wf,
+				obs:                resp.obs,
+				json_procesos:      Ext.util.JSON.encode(resp.procesos)
+				},
+			success:this.successWizard,
+			failure: this.conexionFailure,
+			argument:{wizard:wizard},
+			timeout:this.timeout,
+			scope:this
+		});
+	},
+	
+	successWizard:function(resp){
+		Phx.CP.loadingHide();
+		resp.argument.wizard.panel.destroy()
+		this.reload();
+	 },
+};
+</script>
