23870c17d1c8a890d0cb0c1fb998cc002a111aa1
Jaime Rivera <jriverarojas@gmail.com>
2016-06-06 16:45:56 -0400

    datos basicos del sistema

---
+++ base/data0000001.sql
@@ -0,0 +1,24 @@
+
+/************************************I-DAT-JRR-INFORMIX-0-31/05/2016*************************************************/
+INSERT INTO informix.tmigracion ("id_usuario_reg", "id_usuario_mod", "fecha_reg", "fecha_mod", "estado_reg", "id_usuario_ai", "usuario_ai", "id_migracion", "nombre_tabla", "nombre_funcion", "fecha_ultima_migracion", "fecha_ultimo_intento_migracion", "fecha_hora_ultimo_intento", "tipo_ultimo_resultado", "ultimo_mensaje", "orden", "tipo")
+VALUES (1, NULL, E'2016-04-28 18:13:00', E'2016-04-28 18:13:00', E'activo', NULL, NULL, 1, E'obingresos.tagencia', E'informix.f_migra_agencia', E'2016-01-01', E'2016-01-01', E'2016-06-05 00:00:00', E'exito', E'exito', 3, E'parametrica');
+
+INSERT INTO informix.tmigracion ("id_usuario_reg", "id_usuario_mod", "fecha_reg", "fecha_mod", "estado_reg", "id_usuario_ai", "usuario_ai", "id_migracion", "nombre_tabla", "nombre_funcion", "fecha_ultima_migracion", "fecha_ultimo_intento_migracion", "fecha_hora_ultimo_intento", "tipo_ultimo_resultado", "ultimo_mensaje", "orden", "tipo")
+VALUES (1, NULL, E'2016-04-29 17:09:09', E'2016-04-29 17:09:09', E'activo', NULL, NULL, 2, E'obingresos.tboleto', E'informix.f_migra_boleto', E'2016-01-01', E'2016-01-01', E'2016-06-05 00:00:00', E'exito', E'exito', 1, E'transaccional');
+
+INSERT INTO informix.tmigracion ("id_usuario_reg", "id_usuario_mod", "fecha_reg", "fecha_mod", "estado_reg", "id_usuario_ai", "usuario_ai", "id_migracion", "nombre_tabla", "nombre_funcion", "fecha_ultima_migracion", "fecha_ultimo_intento_migracion", "fecha_hora_ultimo_intento", "tipo_ultimo_resultado", "ultimo_mensaje", "orden", "tipo")
+VALUES (1, NULL, E'2016-04-29 18:08:31', E'2016-04-29 18:08:31', E'activo', NULL, NULL, 3, E'obingresos.tcomision', E'informix.f_migra_comision', E'2016-01-01', E'2016-01-01', E'2016-06-05 00:00:00', E'exito', E'exito', 4, E'parametrica');
+
+INSERT INTO informix.tmigracion ("id_usuario_reg", "id_usuario_mod", "fecha_reg", "fecha_mod", "estado_reg", "id_usuario_ai", "usuario_ai", "id_migracion", "nombre_tabla", "nombre_funcion", "fecha_ultima_migracion", "fecha_ultimo_intento_migracion", "fecha_hora_ultimo_intento", "tipo_ultimo_resultado", "ultimo_mensaje", "orden", "tipo")
+VALUES (1, NULL, E'2016-05-04 18:24:01', E'2016-05-04 18:24:01', E'activo', NULL, NULL, 4, E'obingresos.timpuesto', E'informix.f_migra_impuesto', E'2016-01-01', E'2016-01-01', E'2016-06-05 00:00:00', E'exito', E'exito', 5, E'parametrica');
+
+INSERT INTO informix.tmigracion ("id_usuario_reg", "id_usuario_mod", "fecha_reg", "fecha_mod", "estado_reg", "id_usuario_ai", "usuario_ai", "id_migracion", "nombre_tabla", "nombre_funcion", "fecha_ultima_migracion", "fecha_ultimo_intento_migracion", "fecha_hora_ultimo_intento", "tipo_ultimo_resultado", "ultimo_mensaje", "orden", "tipo")
+VALUES (1, NULL, E'2016-05-06 15:53:49', E'2016-05-06 15:53:49', E'activo', NULL, NULL, 5, E'obingresos.tforma_pago', E'informix.f_migra_forma_pago', E'2016-01-01', E'2016-01-01', E'2016-06-05 00:00:00', E'exito', E'exito', 6, E'parametrica');
+
+INSERT INTO informix.tmigracion ("id_usuario_reg", "id_usuario_mod", "fecha_reg", "fecha_mod", "estado_reg", "id_usuario_ai", "usuario_ai", "id_migracion", "nombre_tabla", "nombre_funcion", "fecha_ultima_migracion", "fecha_ultimo_intento_migracion", "fecha_hora_ultimo_intento", "tipo_ultimo_resultado", "ultimo_mensaje", "orden", "tipo")
+VALUES (1, NULL, E'2016-06-04 15:17:39', E'2016-06-04 15:17:39', E'activo', NULL, NULL, 7, E'param.tlugar_pais', E'informix.f_migra_pais', E'2016-01-01', E'2016-01-01', E'2016-06-05 00:00:00', E'exito', E'exito', 1, E'parametrica');
+
+INSERT INTO informix.tmigracion ("id_usuario_reg", "id_usuario_mod", "fecha_reg", "fecha_mod", "estado_reg", "id_usuario_ai", "usuario_ai", "id_migracion", "nombre_tabla", "nombre_funcion", "fecha_ultima_migracion", "fecha_ultimo_intento_migracion", "fecha_hora_ultimo_intento", "tipo_ultimo_resultado", "ultimo_mensaje", "orden", "tipo")
+VALUES (1, NULL, E'2016-06-04 15:17:39', E'2016-06-04 15:17:39', E'activo', NULL, NULL, 8, E'param.tlugar_ciudad', E'informix.f_migra_ciudad', E'2016-01-01', E'2016-01-01', E'2016-06-05 00:00:00', E'exito', E'exito', 2, E'parametrica');
+
+/************************************F-DAT-JRR-INFORMIX-0-31/05/2016*************************************************/
