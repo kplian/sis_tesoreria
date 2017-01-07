1e518564efef45cf52f1a53b87070987771cc137
gonzaloSarmientoSejas <gonzalo@kplian.com>
2017-01-07 15:25:20 -0400

    integracion libro de bancos

--- base/funciones/tes.f_integracion_libro_bancos.sql
+++ base/funciones/tes.f_integracion_libro_bancos.sql
@@ -16,7 +16,7 @@ BEGIN
 
   v_resp = 'false';
   raise notice 'integra lb id_int_comprobante %',p_id_int_comprobante; 
-  --gonzalo insercion de cheque en libro bancos
+  --insercion de cheque en libro bancos
   select dpc.prioridad as prioridad_conta,
   		 dpl.prioridad as prioridad_libro,
          tra.forma_pago,
@@ -27,56 +27,60 @@ BEGIN
   left join param.tdepto dpc on dpc.id_depto = cp.id_depto
   left join tes.tdepto_cuenta_bancaria dpcb on dpcb.id_cuenta_bancaria=tra.id_cuenta_bancaria
   left join param.tdepto dpl on dpl.id_depto =dpcb.id_depto
-  --left join param.tdepto dpl on dpl.id_depto = cp.id_depto_libro
-  inner join conta.tplantilla_comprobante pla on pla.id_plantilla_comprobante=cp.id_plantilla_comprobante
+  left join conta.tplantilla_comprobante pla on pla.id_plantilla_comprobante=cp.id_plantilla_comprobante
   where cp.id_int_comprobante = p_id_int_comprobante;
-  
+
   IF v_registros.prioridad_libro is null THEN
   	raise exception 'El comprobante no cuenta con id_depto libro de bancos';
   END IF;
-  
+
   IF v_registros.prioridad_conta is null THEN
   	raise exception 'El comprobante no cuenta con id_depto contabilidad';
   END IF;
-  
+
   select fin.id_finalidad into v_id_finalidad
   from tes.tfinalidad fin
   where fin.nombre_finalidad ilike 'proveedores';
 
   IF (v_registros.forma_pago = 'cheque')THEN
-         
-    if(v_registros.prioridad_conta in (0,1) and v_registros.prioridad_libro not in (0,1))then                        		
-        v_respuesta_libro_bancos = tes.f_generar_deposito_cheque(p_id_usuario,p_id_int_comprobante, v_id_finalidad,NULL,'','nacional');	
+
+    if(v_registros.prioridad_conta in (0,1) and v_registros.prioridad_libro not in (2,3))then
+        v_respuesta_libro_bancos = tes.f_generar_deposito_cheque(p_id_usuario,p_id_int_comprobante, v_id_finalidad,NULL,'','nacional');
         v_resp= 'true';
-    elseif(v_registros.prioridad_conta = 2 and v_registros.prioridad_libro =2 )then	
-        v_respuesta_libro_bancos = tes.f_generar_cheque(p_id_usuario,p_id_int_comprobante, v_id_finalidad,NULL,'','nacional');      
+    elseif(v_registros.prioridad_conta = 2 and v_registros.prioridad_libro =2 )then
+        v_respuesta_libro_bancos = tes.f_generar_cheque(p_id_usuario,p_id_int_comprobante, v_id_finalidad,NULL,'','nacional');
         v_resp= 'true';
-    elseif(v_registros.prioridad_conta in (0,1) and v_registros.prioridad_libro =2 )then	
-        v_respuesta_libro_bancos = tes.f_generar_deposito_cheque(p_id_usuario,p_id_int_comprobante, v_id_finalidad,NULL,'','nacional');      
+    elseif(v_registros.prioridad_conta in (0,1) and v_registros.prioridad_libro =2 )then
+        v_respuesta_libro_bancos = tes.f_generar_deposito_cheque(p_id_usuario,p_id_int_comprobante, v_id_finalidad,NULL,'','nacional');
         v_resp= 'true';
-    elseif(v_registros.prioridad_conta = 3 and v_registros.prioridad_libro =3 )then	
-        --v_respuesta_libro_bancos = tes.f_generar_cheque(p_id_usuario,p_id_int_comprobante, v_id_finalidad,NULL,'','internacional');      
-        v_resp= 'true';     
+    elseif(v_registros.prioridad_conta = 3 and v_registros.prioridad_libro =3 )then
+        --v_respuesta_libro_bancos = tes.f_generar_cheque(p_id_usuario,p_id_int_comprobante, v_id_finalidad,NULL,'','internacional');
+        v_resp= 'true';
     elsif(v_registros.prioridad_conta in (0,1) and v_registros.prioridad_libro in (0,1))then
-    	 /*
+
          select ctab.centro into v_centro
          from conta.tint_transaccion tra
          inner join tes.tcuenta_bancaria ctab on ctab.id_cuenta_bancaria=tra.id_cuenta_bancaria
          where tra.id_int_comprobante=p_id_int_comprobante;
-         
-         IF v_centro='esp' then
-    		v_respuesta_libro_bancos = tes.f_generar_deposito_cheque(p_id_usuario,p_id_int_comprobante, v_id_finalidad,NULL,'','nacional');	
-    	 END IF;
-         */
-         
+
          IF v_registros.codigo in ('REPOCAJA','SOLFONDAV','CIERRE_CAJA') THEN
-         	                   
+
             select fin.id_finalidad into v_id_finalidad
             from tes.tfinalidad fin
             where fin.nombre_finalidad ilike 'Fondo Rotativo';
-            
-         	v_respuesta_libro_bancos = tes.f_generar_cheque(p_id_usuario,p_id_int_comprobante, v_id_finalidad,NULL,'','nacional');
+
+            v_respuesta_libro_bancos = tes.f_generar_cheque(p_id_usuario,p_id_int_comprobante, v_id_finalidad,NULL,'','nacional');
+         ELSE
+         	select fin.id_finalidad into v_id_finalidad
+            from tes.tfinalidad fin
+            where fin.nombre_finalidad ilike 'Proveedores';
+            IF v_centro='esp' then
+    			v_respuesta_libro_bancos = tes.f_generar_deposito_cheque(p_id_usuario,p_id_int_comprobante, v_id_finalidad,NULL,'','nacional');
+            END IF;
+
          END IF;
+
+
          
          v_resp = 'true';
         
