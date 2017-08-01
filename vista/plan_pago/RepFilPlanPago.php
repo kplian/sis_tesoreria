4693ff2f1ed6523888f9ebc51d6b4fedfc12597f
Rensi Arteaga <rensi4rn@gmail.com>
2017-08-01 16:24:10 -0400

    reporte de plan de pago mejorado

--- vista/plan_pago/RepFilPlanPago.php
+++ vista/plan_pago/RepFilPlanPago.php
@@ -34,6 +34,19 @@ Phx.vista.RepFilPlanPago=Ext.extend(Phx.frmInterfaz,{
         if(config.id_obligacion_pago > 0){
         	this.cargarDatosFiltro(config.id_obligacion_pago);
         }
+        
+        if(config.detalle){
+        	console.log('detalle a cargar en filtro', config.detalle)
+			//cargar los valores para el filtro
+			this.loadForm({data: config.detalle});
+			var me = this;
+			setTimeout(function(){
+				me.onSubmit()
+			}, 1500);
+			
+		} 
+        
+        
     },
     
     cargarDatosFiltro: function(id_obligacion_pago){
@@ -102,7 +115,7 @@ Phx.vista.RepFilPlanPago=Ext.extend(Phx.frmInterfaz,{
 				forceSelection: true,
 				typeAhead: true,
 				listWidth:'280',
-				width: 150,
+				width:220,
 				mode: 'remote'
 			},
 	           			
@@ -144,9 +157,8 @@ Phx.vista.RepFilPlanPago=Ext.extend(Phx.frmInterfaz,{
                mode:'remote',
                pageSize:10,
                queryDelay:1000,
-               width: 150,
+               width:220,
                listWidth:'280',
-               gwidth:350,
                minChars:2,
 	       	   enableMultiSelect: true
             },
@@ -187,8 +199,7 @@ Phx.vista.RepFilPlanPago=Ext.extend(Phx.frmInterfaz,{
                mode:'remote',
                pageSize:10,
                queryDelay:1000,
-               width: 150,
-               gwidth:200,
+               width:220,
                minChars:2,
 	       	   enableMultiSelect:true
             },
@@ -201,11 +212,152 @@ Phx.vista.RepFilPlanPago=Ext.extend(Phx.frmInterfaz,{
 				name: 'nro_tramite',
 				allowBlank: true,
 				fieldLabel: 'Nro. Trámite',
-				width: 150
+				width: 220
 			},
 			type: 'Field',
 			id_grupo: 0,
 			form: true
+		},
+		{
+				config:{
+					name: 'desde',
+					fieldLabel: 'Desde',
+					allowBlank: true,
+					format: 'd/m/Y',
+					width:220,
+				},
+				type: 'DateField',
+				id_grupo: 0,
+				form: true
+		  },
+		  {
+				config:{
+					name: 'hasta',
+					fieldLabel: 'Hasta',
+					allowBlank: true,
+					format: 'd/m/Y',
+					width:220,
+				},
+				type: 'DateField',
+				id_grupo: 0,
+				form: true
+		  },
+		  {
+			config : {
+						name:'tipo_pago',
+						qtip:'Tipos de pago que se queiren revisar',
+						fieldLabel : 'Tipo de Pago',
+						resizable:true,
+						allowBlank:true,
+		   				store: new Ext.data.JsonStore({
+							url: '../../sis_tesoreria/control/TipoPlanPago/listarTipoPlanPago',
+							id: 'id_tipo_plan_pago',
+							root: 'datos',
+							sortInfo:{
+								field: 'codigo',
+								direction: 'ASC'
+							},
+							totalProperty: 'total',
+							fields: ['id_tipo_plan_pago','codigo','descripcion'],
+							// turn on remote sorting
+							remoteSort: true,
+							baseParams: {par_filtro:'descripcion#codigo'}
+						}),
+	       			    enableMultiSelect:true,    				
+						valueField: 'codigo',
+		   				displayField: 'descripcion',
+		   				gdisplayField: 'desc_tipo_pago',
+		   				forceSelection:true,
+		   				typeAhead: false,
+		       			triggerAction: 'all',
+		       			lazyRender:true,
+		   				mode:'remote',
+		   				pageSize:10,
+		   				queryDelay:1000,
+		   				width:220,
+		   				minChars:2
+		    },
+			type : 'AwesomeCombo',
+			form : true
+		},
+		{
+			config : {
+						name:'estado',
+						qtip:'todos los pagos que esten en alguno de los estados seleccionados',
+						fieldLabel : 'En estado:',
+						resizable:true,
+						allowBlank:true,
+		   				emptyText:'Seleccione un catálogo...',
+		   				store: new Ext.data.JsonStore({
+							url: '../../sis_parametros/control/Catalogo/listarCatalogoCombo',
+							id: 'id_catalogo',
+							root: 'datos',
+							sortInfo:{
+								field: 'orden',
+								direction: 'ASC'
+							},
+							totalProperty: 'total',
+							fields: ['id_catalogo','codigo','descripcion'],
+							// turn on remote sorting
+							remoteSort: true,
+							baseParams: {par_filtro:'descripcion',cod_subsistema:'TES',catalogo_tipo:'tplan_pago'}
+						}),
+	       			    enableMultiSelect:true,    				
+						valueField: 'codigo',
+		   				displayField: 'descripcion',
+		   				gdisplayField: 'catalogo',
+		   				forceSelection:true,
+		   				typeAhead: false,
+		       			triggerAction: 'all',
+		       			lazyRender:true,
+		   				mode:'remote',
+		   				pageSize:10,
+		   				queryDelay:1000,
+		   				width:220,
+		   				minChars:2
+		    },
+			type : 'AwesomeCombo',
+			form : true
+		 },
+		 {
+			config : {
+						name:'fuera_estado',
+						qtip:'todos los pagos que NO esten en alguno de los estados seleccionados',
+						fieldLabel : 'No estado:',
+						resizable:true,
+						allowBlank:true,
+		   				emptyText:'Seleccione un catálogo...',
+		   				store: new Ext.data.JsonStore({
+							url:'../../sis_parametros/control/Catalogo/listarCatalogoCombo',
+							id: 'id_catalogo',
+							root: 'datos',
+							sortInfo:{
+								field: 'orden',
+								direction: 'ASC'
+							},
+							totalProperty: 'total',
+							fields: ['id_catalogo','codigo','descripcion'],
+							// turn on remote sorting
+							remoteSort: true,
+							baseParams: {par_filtro:'descripcion',cod_subsistema:'TES',catalogo_tipo:'tplan_pago'}
+						}),
+	       			    enableMultiSelect:true,    				
+						valueField: 'codigo',
+		   				displayField: 'descripcion',
+		   				gdisplayField: 'catalogo',
+		   				hiddenName: 'catalogo',
+		   				forceSelection:true,
+		   				typeAhead: false,
+		       			triggerAction: 'all',
+		       			lazyRender:true,
+		   				mode:'remote',
+		   				pageSize:10,
+		   				queryDelay:1000,
+		   				width:220,
+		   				minChars:2
+		    },
+			type : 'AwesomeCombo',
+			form : true
 		}
 
     ],
