94edea7b215370152bc0ed72b59021c9f26d59fb
Rodrigo Chumacero <rodrychm@gmail.com>
2019-01-03 17:02:57 -0400

    #10 endeetr master la fecha de reposicion o devolucion afectara segun la gestion

--- base/funciones/tes.f_trig_solicitud_efectivo_before.sql
+++ base/funciones/tes.f_trig_solicitud_efectivo_before.sql
@@ -4,6 +4,13 @@ CREATE OR REPLACE FUNCTION tes.f_trig_solicitud_efectivo_before (
 )
 RETURNS trigger AS
 $body$
+/***************************************************************************
+     HISTORIAL DE MODIFICACIONES:
+    
+ ISSUE    FORK        FECHA:          AUTOR                 DESCRIPCION
+
+ #10    ENDEETR      03/01/2019        Manu Guerra     		la fecha de reposicion o devolucion afectara segun la gestion 
+***************************************************************************/
 DECLARE
   v_registros_con 	 record;
   v_registros		 record;
@@ -15,7 +22,12 @@ BEGIN
  
     
     IF  new.estado in ( 'entregado','finalizado','repuesto','ingresado','devuelto','aperturado','rendido')  and NEW.fecha_ult_mov is null THEN
-         NEW.fecha_ult_mov = now();
+        if EXTRACT (year from old.fecha)= EXTRACT (year from now()) then
+        	NEW.fecha_ult_mov = now();
+        else	
+        	NEW.fecha_ult_mov = '31/12/2018'::date;
+        end if;  
+        
     END IF;
     
    RETURN NEW;
