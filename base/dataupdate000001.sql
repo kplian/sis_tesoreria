0619234c681fdb4b5c103d36be594d06930d5106
mguerra <manuel.guerra@endetransmision.bo>
2020-09-30 20:19:33 -0400

    #ETR-1180 cambiar el monto de rendicion

--- base/dataupdate000001.sql
+++ base/dataupdate000001.sql
@@ -15,3 +15,12 @@ UPDATE tes.tsolicitud_efectivo SET monto=0 WHERE id_solicitud_efectivo =32300;
 --commit
 UPDATE tes.tsolicitud_efectivo SET monto=1776.10 WHERE id_solicitud_efectivo =32297;
 /********************************************F-DAUP-MGM-TES-0-30/09/2020********************************************/
+
+
+/********************************************I-DAUP-MGM-TES-1-30/09/2020********************************************/
+--rollback
+--UPDATE tes.tsolicitud_rendicion_det SET monto=1578.76 WHERE id_solicitud_efectivo =21617;
+--commit
+UPDATE tes.tsolicitud_rendicion_det SET monto=1776.10 WHERE id_solicitud_efectivo =21617;
+
+/********************************************F-DAUP-MGM-TES-1-30/09/2020********************************************/
