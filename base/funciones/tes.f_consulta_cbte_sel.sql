f3cc10c1af0b19a52849992f9999f23bc87fa970
amamani <alejandro.mamani@endetransmision.bo>
2021-03-12 09:26:16 -0400

    ETR-3127 : Reporte para VoBo Cbte (Finanzas) Refac
    Conflicts:
    	base/funciones/tes.f_consulta_cbte_sel.sql

--- base/funciones/tes.f_consulta_cbte_sel.sql
+++ base/funciones/tes.f_consulta_cbte_sel.sql
@@ -1,12 +1,8 @@
-CREATE
-    OR REPLACE FUNCTION tes.f_consulta_cbte_sel(
-    p_administrador integer,
-    p_id_usuario integer,
-    p_tabla varchar,
-    p_transaccion varchar
-)
-    RETURNS varchar AS
-$body$
+create or replace function tes.f_consulta_cbte_sel(p_administrador integer, p_id_usuario integer, p_tabla character varying,
+                                    p_transaccion character varying) returns character varying
+    language plpgsql
+as
+$$
 /**************************************************************************
  SISTEMA:		Sistema de Tesoreria
  FUNCION: 		tes.f_consulta_cbte_sel
@@ -71,7 +67,7 @@ BEGIN
               and (uof.fecha_finalizacion is null or uof.fecha_finalizacion >= now()::date)
               and usu.id_usuario = p_id_usuario;
             IF
-                v_codigo = 'DTE' or v_codigo = 'GAF' THEN
+                        v_codigo = 'DTE' or v_codigo = 'GAF' THEN
                 WITH RECURSIVE uo_centro(
                                          ids,
                                          id_uo,
@@ -103,47 +99,69 @@ BEGIN
             END IF;
 
             IF
-                p_administrador != 1 THEN
+                    p_administrador != 1 THEN
                 --#74
                 IF v_id_funcionarios IS NOT NULL THEN
-                    v_filtro =
-                            ' (lower(incbte.estado_reg)!=''borrador'') and (lower(incbte.estado_reg)!=''validado'' ) and ';
+                    v_filtro = ' (lower(cbte.estado_reg)!=''borrador'') and (lower(cbte.estado_reg)!=''validado'' ) ';
                 ELSE
-                    v_filtro = ' (ew.id_funcionario=' || v_parametros.id_funcionario_usu::varchar ||
-                               ') and (lower(incbte.estado_reg)!=''anulado'') and (lower(incbte.estado_reg)!=''borrador'') and (lower(incbte.estado_reg)!=''validado'' ) and ';
+                    v_filtro =
+                            ' (lower(cbte.estado_reg)!=''anulado'') and (lower(cbte.estado_reg)!=''borrador'') and (lower(cbte.estado_reg)!=''validado'' ) ';
                 END IF;
             ELSE
-                v_filtro =
-                        ' (lower(incbte.estado_reg)!=''borrador'') and (lower(incbte.estado_reg)!=''validado'' ) and ';
+                v_filtro = '(lower(cbte.estado_reg)!=''borrador'') and (lower(cbte.estado_reg)!=''validado'' ) ';
             END IF;
 
             --Sentencia de la consulta
             v_consulta
-                := 'select  incbte.manual,
-                                 incbte.estado_reg,
-                                 incbte.id_int_comprobante,
-                                 incbte.fecha,
-                                 incbte.nro_tramite,
-                                 incbte.beneficiario,
-                                 incbte.tipo_cambio,
-                                 incbte.liquido_pagable,
-                                 incbte.glosa1 as glosa,
-                                 pan.fecha_documento,
-                                 (select pxp.list(distinct cc.codigo_cc)
-                                  from tes.tobligacion_det od
-                                           inner join param.vcentro_costo cc on cc.id_centro_costo = od.id_centro_costo
-                                  where od.id_obligacion_pago = pan.id_obligacion_pago) as cento_consto
-                          from conta.vint_comprobante incbte
-                          left join tes.tplan_pago pan on pan.id_int_comprobante = incbte.id_int_comprobante
-                          where  incbte.estado_reg in (''borrador'',''validado'') and ' || v_filtro;
+                := 'select          incbte.manual,
+                                    incbte.estado_reg,
+                                    incbte.id_int_comprobante,
+                                    incbte.fecha,
+                                    incbte.nro_tramite,
+                                    incbte.beneficiario,
+                                    incbte.tipo_cambio,
+                                    incbte.liquido_pagable,
+                                    incbte.glosa,
+                                    incbte.fecha_documento,
+                                    incbte.cento_consto,
+                                    incbte.codigo,
+                                    incbte.temporal,
+                                    incbte.vbregional,' ||
+                   '  incbte.id_depto,
+                      incbte.id_gestion
+      from (
+      select cbte.manual,
+              cbte.estado_reg,
+              cbte.id_int_comprobante,
+              cbte.fecha,
+              cbte.nro_tramite,
+              cbte.beneficiario,
+              cbte.tipo_cambio,
+              cbte.liquido_pagable,
+              cbte.glosa1 as glosa,
+              cbte.temporal, cbte.vbregional,
+              pan.fecha_documento,
+              cbte.id_depto,
+              cbte.id_gestion,
+              (select pxp.list(distinct cc.codigo_cc)
+               from tes.tobligacion_det od
+                        inner join param.vcentro_costo cc on cc.id_centro_costo = od.id_centro_costo
+               where od.id_obligacion_pago = pan.id_obligacion_pago) as cento_consto,
+              mo.codigo
+       from conta.vint_comprobante cbte
+                join param.tmoneda mo on mo.id_moneda = cbte.id_moneda
+                inner join wf.tproceso_wf pwf on pwf.id_proceso_wf = cbte.id_proceso_wf
+                inner join wf.testado_wf ew on ew.id_estado_wf = cbte.id_estado_wf
+                left join tes.tplan_pago pan on pan.id_int_comprobante = cbte.id_int_comprobante
+       where ' || v_filtro || ' ) incbte
+                     where ';
 
             --Definicion de la respuesta
-            v_consulta
-                := v_consulta || v_parametros.filtro;
-            v_consulta
-                := v_consulta || ' order by ' || v_parametros.ordenacion || ' ' || v_parametros.dir_ordenacion ||
-                   ' limit ' || v_parametros.cantidad || ' offset ' || v_parametros.puntero;
+            v_consulta := v_consulta || v_parametros.filtro;
+            v_consulta := v_consulta || ' order by ' || v_parametros.ordenacion || ' ' || v_parametros.dir_ordenacion ||
+                          ' limit ' || v_parametros.cantidad || ' offset ' || v_parametros.puntero;
             --Devuelve la respuesta
+            raise notice '%',v_consulta;
             return v_consulta;
 
         end;
@@ -178,7 +196,7 @@ BEGIN
               and (uof.fecha_finalizacion is null or uof.fecha_finalizacion >= now()::date)
               and usu.id_usuario = p_id_usuario;
             IF
-                v_codigo = 'DTE' or v_codigo = 'GAF' THEN
+                        v_codigo = 'DTE' or v_codigo = 'GAF' THEN
                 WITH RECURSIVE uo_centro(
                                          ids,
                                          id_uo,
@@ -210,25 +228,46 @@ BEGIN
             END IF;
 
             IF
-                p_administrador != 1 THEN
+                    p_administrador != 1 THEN
                 --#74
                 IF v_id_funcionarios IS NOT NULL THEN
-                    v_filtro =
-                            ' (lower(incbte.estado_reg)!=''borrador'') and (lower(incbte.estado_reg)!=''validado'' ) and ';
+                    v_filtro = ' (lower(cbte.estado_reg)!=''borrador'') and (lower(cbte.estado_reg)!=''validado'' ) ';
                 ELSE
-                    v_filtro = ' (ew.id_funcionario=' || v_parametros.id_funcionario_usu::varchar ||
-                               ') and (lower(incbte.estado_reg)!=''anulado'') and (lower(incbte.estado_reg)!=''borrador'') and (lower(incbte.estado_reg)!=''validado'' ) and ';
+                    v_filtro =
+                            ' (lower(cbte.estado_reg)!=''anulado'') and (lower(cbte.estado_reg)!=''borrador'') and (lower(cbte.estado_reg)!=''validado'' ) ';
                 END IF;
             ELSE
-                v_filtro =
-                        ' (lower(incbte.estado_reg)!=''borrador'') and (lower(incbte.estado_reg)!=''validado'' ) and ';
+                v_filtro = '(lower(cbte.estado_reg)!=''borrador'') and (lower(cbte.estado_reg)!=''validado'' ) ';
             END IF;
             --Sentencia de la consulta de conteo de registros
             v_consulta
-                := 'select count(incbte.id_int_comprobante)
-                        from conta.vint_comprobante incbte
-                        left join tes.tplan_pago pan on pan.id_int_comprobante = incbte.id_int_comprobante
-                         where  incbte.estado_reg in (''borrador'',''validado'') and' || v_filtro;
+                := 'select  count(incbte.id_int_comprobante)
+                    from (
+                    select cbte.manual,
+                            cbte.estado_reg,
+                            cbte.id_int_comprobante,
+                            cbte.fecha,
+                            cbte.nro_tramite,
+                            cbte.beneficiario,
+                            cbte.tipo_cambio,
+                            cbte.liquido_pagable,
+                            cbte.glosa1 as glosa,
+                            cbte.temporal, cbte.vbregional,
+                            pan.fecha_documento,
+                            cbte.id_depto,
+                            cbte.id_gestion,
+                            (select pxp.list(distinct cc.codigo_cc)
+                             from tes.tobligacion_det od
+                                      inner join param.vcentro_costo cc on cc.id_centro_costo = od.id_centro_costo
+                             where od.id_obligacion_pago = pan.id_obligacion_pago) as cento_consto,
+                            mo.codigo
+                     from conta.vint_comprobante cbte
+                              inner join param.tmoneda mo on mo.id_moneda = cbte.id_moneda
+                              inner join wf.tproceso_wf pwf on pwf.id_proceso_wf = cbte.id_proceso_wf
+                              inner join wf.testado_wf ew on ew.id_estado_wf = cbte.id_estado_wf
+                              left join tes.tplan_pago pan on pan.id_int_comprobante = cbte.id_int_comprobante
+                     where ' || v_filtro || ' ) incbte
+                     where ';
             --Definicion de la respuesta
             v_consulta
                 := v_consulta || v_parametros.filtro;
@@ -257,10 +296,4 @@ EXCEPTION
         raise
             exception '%',v_resp;
 END;
-$body$
-    LANGUAGE 'plpgsql'
-    VOLATILE
-    CALLED ON NULL INPUT
-    SECURITY INVOKER
-    PARALLEL UNSAFE
-    COST 100;
+$$;
